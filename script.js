1d9b1b34098a45e20d66c89d6fffcd914731748a
Strongie <nickstrong665@gmail.com>
2022-08-31 20:59:15 +1000

    storing to local storage

--- script.js
+++ script.js
@@ -5,11 +5,34 @@ $("#currentDay").text(today.format("MMM Do, YYYY"));
 
 // set differing time slots
 //var dailyTime = document.getElementById("inputGroup-sizing-default");
-var time = "";
-for (let i = 0 + 9; i < 18; i++) {
-  time += i + ".00" + "<br>";
-}
+//var time = "";
+//for (let i = 0 + 9; i < 18; i++) {
+ // time += i + ".00" + "<br>";
+//}
 
-document.getElementById("inputGroup-sizing-default").innerHTML = time;
+//document.getElementById("hours8").innerHTML = time;
+
+//get the correct format for moment and set it equal to a variable 
+//compare that to the id of the hour (9 am ,  10am 11am)
+//will be using this to target the hour
+//write if else if statment comparent the to and change the attribute depending on the color (set attribute of class past)
+
+//local storage to save the notes 
+//use text and hour
+
+ //var hour= moment().hour(Number);
+ //Number = 10
+ //console.log(hour)
 
 
+ // store the activities in local storage
+
+ var saveButtonEL = document.querySelector('#saveBtn');
+ 
+ function storeActivity() {
+    var input = document.getElementById("input2").value;
+    localStorage.setItem("dailyactivity", input);
+ }
+ saveButtonEL.onclick = () => {
+    storeActivity();
+  }
